31fc7d89ab20b0336a9e3c18a7e829e9b7c431d9
Dayanand <dayanandnaykude2303@gmail.com>
2024-01-30 22:35:27 +0530

    Learnt for loop

---
+++ Day-5/main.py
@@ -0,0 +1,14 @@
+fruits = ["Apple","Peach", "Pear"]
+
+for fruit in fruits:
+    print(fruit)
+
+
+# range funtion
+
+total = 0
+
+for number in range(1, 101):
+    total += number
+
+print(f" Sum of intgers form 1 to 100 = {total}")
