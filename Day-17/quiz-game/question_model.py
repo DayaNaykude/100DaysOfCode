02151f25c04a89d1d52c2c235c0b3a623ff31a7a
Dayanand <dayanandnaykude2303@gmail.com>
2024-02-11 17:07:28 +0530

    quiz-game using oops concept

---
+++ Day-17/quiz-game/question_model.py
@@ -0,0 +1,6 @@
+class Question:
+    """ class for question model which accepts text and answer as parameters for construct question"""
+
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
